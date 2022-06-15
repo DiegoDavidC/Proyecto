296e4a9a73dcf9bb5fc9ded05a6583e9b00ad408
Diego Carvajal <ddcarvajal@poligran.edu.co>
2022-06-14 23:14:53 -0500

    Se filtro el codigo, para que no estuviera repetido y generara errores

--- main.java
+++ main.java
@@ -1,11 +1,31 @@
 import java.util.Random;
 import java.util.Scanner;
 
-
-
 public class main {
 
 	public static void main (String[] args) {
+
+		Tablero tablero = new Tablero();
+        tablero.imprimir();
+        Jugador jugador = new Jugador();
+        boolean ganador = false;
+
+        for (int i = 1; i <= tablero.getCantidadElementos() * 2; i++) {
+            String letra = jugador.pedirLetra();
+            tablero.verificar(letra);
+            tablero.imprimir();
+            if (tablero.verificarGanador()) {
+                ganador = true;
+                break;
+            }
+        }
+        if (ganador) {
+            System.out.println("HAZ GANADO!!!");
+           
+        }else{
+            System.out.println("VUELVE A INTENTARLO!!!");
+        }
+
 		String [] palabras = {"casa", "avion" , "zapato" ,"vaca"}; 
 		Random r = new Random();
 		int n = r.nextInt(palabras.length);
